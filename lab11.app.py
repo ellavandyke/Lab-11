8ea74b9568c48ea17e47647f368441b72b805f22
ellavandyke <spb115@mocs.utc.edu>
2025-04-05 00:27:36 -0400

    lab11.app.py

--- lab11.app.py
+++ lab11.app.py
@@ -95,7 +95,7 @@ num_pipeline = Pipeline([
 ])
 cat_pipeline = Pipeline([
     ('imputer', SimpleImputer(strategy='most_frequent')),
-    ('onehot', OneHotEncoder(sparse=False, handle_unknown='ignore'))  
+    ('onehot', OneHotEncoder(sparse=True, handle_unknown='ignore'))  
 ])
 
 # Combine pipelines
